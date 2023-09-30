b6e84061d5abca23fd6f627906e94f25cd0bcb8b
cristobers <72763707+cristobers@users.noreply.github.com>
2023-09-30 22:20:41 +0100

    changed colours, moved some features from main into their own functions

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # list: a D.I.Y. `ls` imitation
 
-
-
 ## What is this?
 This is just a small take on the `ls` command, it shows the group, user file size and
 file name for a given directory.
--- file-reader.c
+++ file-reader.c
@@ -5,6 +5,7 @@
 #include <errno.h>
 #include <limits.h>
 
+#include <time.h>
 #include <dirent.h>
 #include <unistd.h>
 #include <pwd.h>
@@ -17,6 +18,7 @@
 
 void print_directory(DIR* directory, char* path);
 void print_file(char* filename, char* colour);
+void argument_check(int argc, char* argv[]);
 int check_current_directory(int argc, char* argv[]);
 
 enum print_level {
@@ -24,27 +26,14 @@ enum print_level {
     VERBOSE
 };
 enum print_level verbosity = NORMAL;
+const int print_padding = 9; // dont worry about the hardcoded value...
 
 int main(int argc, char* argv[]) { 
     DIR* directory;
     char cwd[PATH_MAX]; 
     char* directory_name = NULL;
 
-    if (argc == 2 || argc == 3) {
-        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
-            printf("./list <args> <path>\n"
-                   "-h, --help:\tdisplays help\n-v, --verbose\tverbose print\n"
-                  );
-            return EXIT_SUCCESS;
-        } else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0) {
-            verbosity = VERBOSE;
-        } else if (argv[1][0] != '-' && argc == 3) {
-            return EXIT_FAILURE; // this is kind of hacky for now
-        }
-    } else if (argc > 3) {
-        printf("Too many arguments given.\n");
-        return EXIT_FAILURE;
-    }
+    argument_check(argc, argv);
 
     // if we should check the current directory
     if (check_current_directory(argc, argv)) {
@@ -80,12 +69,31 @@ int check_current_directory(int argc, char* argv[]) {
          if we dont supply the command with a path, 
          we want to check the current working directory.
      */
-    if (verbosity == VERBOSE && argc == 2) // ./list -v <blank>
-        return 1; 
-    else if (verbosity == NORMAL && argc == 1) // ./list <blank>
+    if (verbosity == VERBOSE && argc == 2) {
         return 1;
-    else
+    } else if (verbosity == NORMAL && argc == 1) {
+        return 1;
+    } else {
         return 0;
+    }
+}
+
+void argument_check(int argc, char* argv[]) {
+    if (argc == 2 || argc == 3) {
+        if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) {
+            printf("./list <args> <path>\n"
+                   "-h, --help:\tdisplays help\n"
+                   "-v, --verbose\tverbose print\n");
+            exit(EXIT_SUCCESS);
+        } else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--verbose") == 0) {
+            verbosity = VERBOSE;
+        } else if (argv[1][0] != '-' && argc == 3) {
+            exit(EXIT_FAILURE); // this is still a little hacky for now
+        }
+    } else if (argc > 3) {
+        printf("Too many arguments given.\n");
+        exit(EXIT_FAILURE);
+    }
 }
 
 void print_directory(DIR* directory, char* path) {
@@ -106,7 +114,8 @@ void print_directory(DIR* directory, char* path) {
         exit(EXIT_FAILURE);
     }
 
-    while ((dir = readdir(directory)) != NULL) {
+    int count = 0;
+    while ((dir = readdir(directory)) != NULL && ++count) {
         switch(dir->d_type) {
             case DT_BLK:
                 print_file(dir->d_name, YEL);
@@ -132,6 +141,9 @@ void print_directory(DIR* directory, char* path) {
                 print_file(dir->d_name, RED);
                 break;
         }
+    } if (verbosity == VERBOSE) {
+        // Prints the amount of files in a directory, excluding ../ and ./
+        printf("%d files in directory\n", count-2);
     }
 }
 
@@ -139,6 +151,7 @@ void print_file(char* filename, char* colour) {
     struct stat file_stats; 
     struct passwd* pwuser;
     struct group* groupuser;
+    char date[40];
 
     if (lstat(filename, &file_stats) == 0) {
         // https://pubs.opengroup.org/onlinepubs/7908799/xsh/pwd.h.html
@@ -154,10 +167,18 @@ void print_file(char* filename, char* colour) {
         }
         
         if (verbosity == VERBOSE) {
-            int width = 10; // dont worry about the hardcoded value...
-            printf(WHT"%s ", groupuser->gr_name);
-            printf(WHT"%s ", pwuser->pw_name);
-            printf(YEL"%*lu ", width, file_stats.st_size);
+            const char* format = "%b %d %Y - %H:%M";
+            strftime(date, 80, format, localtime(&(file_stats.st_ctime)));
+            printf(YEL"%s ", groupuser->gr_name);
+            printf(YEL"%s ", pwuser->pw_name);
+            printf(BLU"%s ", date);
+            /*
+                To nicely print out the filesize (rougly) we can do:
+                file_stats.st_size >> 10 --> bytes to KB
+                and then if we did that again, we would get
+                file_stats.st_size >> 10 --> KB to MB
+            */
+            printf(GRN"%*lu ", print_padding, file_stats.st_size );
         }
         
         if ((strcmp(colour, BLU)) == 0) {
